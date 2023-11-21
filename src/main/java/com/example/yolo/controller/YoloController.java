d1355e7f35b0126891368f68086dddf4641dd42b
Brian Kurilko <briankurilko@gmail.com>
2023-11-21 14:14:55 -0500

    First commit

---
+++ src/main/java/com/example/yolo/controller/YoloController.java
@@ -0,0 +1,16 @@
+package com.example.yolo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class YoloController {
+    @GetMapping("/hello-world")
+    @ResponseBody
+    public String helloWorld() {
+        return "Version 0.0.1";
+    }
+}
---
+++ src/test/java/com/example/yolo/YoloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
